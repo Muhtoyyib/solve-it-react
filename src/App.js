144d97f972b0d53a7825e3d01ae1e7862d3816b3
Akande Olalekan Toheeb <akandeolalekantoheeb94@gmail.com>
2023-03-14 18:58:51 +0100

    Create permutation functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,65 @@
 import './App.css';
 import Header from './Components/Header-component/Header.component';
+import Permutation from './Components/Permutation-component/Permutation.component';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App container-fluid center">
-    <Header />
-    </div>
-  );
+class App extends Component{
+
+  constructor(){
+    super()
+    this.state = {
+      ans: ''
+    }
+  }
+
+  permutation =(n,r)=>{
+  
+    n = document.getElementById('n').value;
+    r = document.getElementById('r').value;
+   
+    function factorial(num){
+      let product = 1;
+    for(let i=2; i <= num; i++){
+      product *= i;
+    }
+     return product;
+    }
+   const ans = factorial(n)/factorial(n-r);
+
+   console.log(ans);
+
+   this.setState(() => {
+    return {ans}
+  })
+   }
+
+  render(){
+    const {permutation} = this;
+
+
+    function displayPermutation(){
+      let element = document.getElementById('permSol');
+      element.classList.remove('d-none');
+    }
+  
+    function hidePermutation(){
+      let element = document.getElementById('permSol');
+      element.classList.add('d-none');
+    }
+  
+   
+  
+  
+    return (
+      <div className="App container-fluid center">
+      <Header />
+      <Permutation displayPermutation={displayPermutation} hidePermutation={hidePermutation} permutate={permutation} ans={this.state.ans}/>
+      </div>
+    );
+  }
+
+  
+ 
 }
 
 export default App;
